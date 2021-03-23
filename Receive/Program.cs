e67e7be8fdebfdd9331c46e9dd1cf02ddc36ec33
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2021-03-23 18:37:27 -0300

    commit inicial

---
+++ Receive/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    private const string EXCHANGE = "eventos";
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(EXCHANGE, ExchangeType.Fanout, durable: true);
+
+            var fila = channel.QueueDeclare();
+
+            channel.QueueBind(fila.QueueName, EXCHANGE, routingKey: "");
+
+            Console.WriteLine(" [*] Aguardando mensagens.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Recebido {0}: {1}", ea.RoutingKey, message);
+            };
+            channel.BasicConsume(fila.QueueName, autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Pressione [enter] para sair.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+using System.Threading;
+
+class Send
+{
+    private const string EXCHANGE = "eventos";
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Pressione [enter] para sair");
+
+        bool sair = false;
+        do
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(EXCHANGE, ExchangeType.Fanout, durable: true);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                var routingKey = GetRoutingKey();
+
+                channel.BasicPublish(EXCHANGE, routingKey, basicProperties: null, body: body);
+                Console.WriteLine(" [x] Enviado {0}", routingKey);
+            }
+            Thread.Sleep(2000);
+            sair = Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter;
+        } while (!sair);
+    }
+
+    private static string GetRoutingKey()
+    {
+        string[] Keys = { "inclusao", "alteracao", "exclusao" };
+        var i = new Random().Next(Keys.Length - 1);
+        return Keys[i];
+    }
+}
