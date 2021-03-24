ff212b6813ffc156f91b7223badfd726280d0fb6
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2021-03-24 11:02:18 -0300

    Criar filas antecipadamente

--- Send/Program.cs
+++ Send/Program.cs
@@ -14,14 +14,15 @@ class Send
 
         bool sair = false;
         int numEvento = 0;
-        do
-        {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.ExchangeDeclare(EXCHANGE, ExchangeType.Fanout, durable: true);
 
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(EXCHANGE, ExchangeType.Fanout, durable: true);
+            CriarFilas(channel);
+            do
+            {
                 numEvento++;
                 string message = $"{numEvento}: Hello World!";
                 var body = Encoding.UTF8.GetBytes(message);
@@ -29,10 +30,22 @@ class Send
 
                 channel.BasicPublish(EXCHANGE, routingKey, basicProperties: null, body: body);
                 Console.WriteLine(" [x] Enviado {0} {1}", numEvento, routingKey);
-            }
-            Thread.Sleep(2000);
-            sair = Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter;
-        } while (!sair);
+
+                Thread.Sleep(2000);
+                sair = Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter;
+            } while (!sair);
+        }
+    }
+
+    private static void CriarFilas(IModel channel)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            var idFila = i.ToString();
+            Console.WriteLine($" Criando fila {idFila}");
+            var fila = channel.QueueDeclare(idFila, durable: true, exclusive: false, autoDelete: false);
+            channel.QueueBind(idFila, EXCHANGE, routingKey: "");
+        }
     }
 
     private static string GetRoutingKey()
